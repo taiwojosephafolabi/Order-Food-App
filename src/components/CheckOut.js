66b8660c576385645d2934909b8e6d2cffe57bc5
ellie <ellie.tms1371@gmail.com>
2021-04-09 00:06:23 +0100

    added new changed of the payment

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -9,8 +9,9 @@ const Checkout = ({GoToShoppingLists}) => {
           Back
         </button>
       </div>
-      <div className="row">
-      you are in!
+      <div>
+      <img src="https://lh3.googleusercontent.com/proxy/JdMHzjmBs4n9xswcihfbEDgfBoh5GJSLqM7moH3pO7Kh1Y7NZ1shPvosR2n6W-keRIZZ9HfXm3ztOH8wsaUnoFzQZVKvnhlbEf2fhyuids31QuhpUexFHdledaX6xs-egDqG2YHuQlnjo2DrKq8LBMvHEA"   />
+      <p className="checkout">🎉Your transaction has been successfully completed🎉</p>
       </div>
     </div>
   );
