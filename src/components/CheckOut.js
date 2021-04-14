cbbaa91ce1eb8a264fbd67a2464b0b17a81c1fb4
ellie <ellie.tms1371@gmail.com>
2021-04-14 10:04:01 +0100

    changed the img

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -10,7 +10,7 @@ const Checkout = ({GoToShoppingLists}) => {
         </button>
       </div>
       <div>
-      <img src="https://lh3.googleusercontent.com/proxy/JdMHzjmBs4n9xswcihfbEDgfBoh5GJSLqM7moH3pO7Kh1Y7NZ1shPvosR2n6W-keRIZZ9HfXm3ztOH8wsaUnoFzQZVKvnhlbEf2fhyuids31QuhpUexFHdledaX6xs-egDqG2YHuQlnjo2DrKq8LBMvHEA" alt="food"  />
+      <img src="https://www.apple.com/v/apple-pay/i/images/overview/og_image.png?202008250327" alt="food"  />
       <p className="checkout">🎉Your transaction has been successfully completed🎉</p>
       </div>
     </div>
