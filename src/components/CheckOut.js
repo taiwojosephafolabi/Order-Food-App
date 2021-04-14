b48210662b2a68a559417b2319f108ee69bd33bd
ellie <ellie.tms1371@gmail.com>
2021-04-14 09:53:11 +0100

    fix error of netlify

--- src/components/CheckOut.js
+++ src/components/CheckOut.js
@@ -10,7 +10,7 @@ const Checkout = ({GoToShoppingLists}) => {
         </button>
       </div>
       <div>
-      <img src="https://lh3.googleusercontent.com/proxy/JdMHzjmBs4n9xswcihfbEDgfBoh5GJSLqM7moH3pO7Kh1Y7NZ1shPvosR2n6W-keRIZZ9HfXm3ztOH8wsaUnoFzQZVKvnhlbEf2fhyuids31QuhpUexFHdledaX6xs-egDqG2YHuQlnjo2DrKq8LBMvHEA"   />
+      <img src="https://lh3.googleusercontent.com/proxy/JdMHzjmBs4n9xswcihfbEDgfBoh5GJSLqM7moH3pO7Kh1Y7NZ1shPvosR2n6W-keRIZZ9HfXm3ztOH8wsaUnoFzQZVKvnhlbEf2fhyuids31QuhpUexFHdledaX6xs-egDqG2YHuQlnjo2DrKq8LBMvHEA" alt="food"  />
       <p className="checkout">🎉Your transaction has been successfully completed🎉</p>
       </div>
     </div>
